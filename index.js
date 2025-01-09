5ead996be02b4a1e8e3b0339ed60dad5db70b791
Grand <monyx5861@gmail.com>
2025-01-09 09:00:44 +0100

    Task 3: Add formatting error

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ export const sum = (a, b) => {
 const word = generate();
 console.log(
   `Hello!!! 
-  The ${chalk.redBright('random word')} of today is: ${chalk.bgRed(word)}. 
+  The ${chalk.redBright("random word")} of today is: ${chalk.bgRed(word)}. 
   The word length is ${sum(word.length, 0)}.`,
 );
